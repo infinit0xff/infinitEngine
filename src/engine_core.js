be5536abaa9a7ab73acd7133f4e6122d803c49e8
infinit0xff <infinit0xff@gmail.com>
2019-09-05 15:03:03 -0700

    initializeWebgl function creates canvas, public vars.

---
+++ src/engine_core.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var infinitEngine = infinitEngine || {};
+
+infinitEngine.Core = (function() {
+    
+    // 'iv' means instance variable
+
+    // graphical context
+    var ivGL = null;
+
+    // accessor for webgl context
+    var getGL = function() { return ivGL; };
+
+    // initialize webgl, vertex buffer and compile shaders.
+    var initializeWebGL = function(htmlCanvasID) {
+
+        // create canvas and expose as HTMLCanvasElement
+        var canvas = document.createElement("canvas");
+        canvas.id = htmlCanvasID;
+        canvas.width = 640;
+        canvas.height = 480;
+        canvas.innerHTML = "your browser does not support HTML5 canvas."
+        
+        // add canvas to body
+        document.querySelector('body').appendChild(canvas);
+
+        ivGL = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+        
+        // if context doesnt load then error
+        if(ivGL === null) {
+            document.write("<br><b>WebGL not supported!</b>");
+            return;
+        }
+    }
+
+    // contains accessible functions and vars
+    var ivPublic = {
+        getGL: getGL
+    };
+
+    return ivPublic;
+}()); // create single instance of gEngine.Core
