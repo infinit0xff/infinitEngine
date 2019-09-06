8f87a4417a684daa13f59ae9d922162255e1deb7
infinit0xff <infinit0xff@gmail.com>
2019-09-05 21:35:21 -0700

    load new shader in demo.js with pixelColor.

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -9,14 +9,14 @@ function Demo(htmlCanvasID) {
     // create, load and compile shader 
     this.ivShader = new SimpleShader(
         "src/glsl_shaders/simple_vs.glsl",
-        "src/glsl_shaders/white_fs.glsl"
+        "src/glsl_shaders/simple_fs.glsl"
     );
     
     // clear the canvas
     infinitEngine.Core.clearCanvas([0, 0.8, 0, 1]);
 
     // activate proper shader
-    this.ivShader.activateShader();
+    this.ivShader.activateShader([0, 0, 1, 1]);
 
     // draw with activated geometry and shader
     var gl = infinitEngine.Core.getGL();
