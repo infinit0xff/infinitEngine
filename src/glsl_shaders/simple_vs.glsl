eb3c777a8bc6df3540bb1da5f9779d7be1136382
infinit0xff <infinit0xff@gmail.com>
2019-09-07 09:43:30 -0700

    modified vertex shader to support view projection transform.

--- src/glsl_shaders/simple_vs.glsl
+++ src/glsl_shaders/simple_vs.glsl
@@ -1,7 +1,8 @@
 attribute vec3 aSquareVertexPosition;
 
+uniform mat4 uViewProjTransform;
 uniform mat4 uModelTransform;
 
 void main(void) {
-    gl_Position = uModelTransform * vec4(aSquareVertexPosition, 1.0);
+    gl_Position = uViewProjTransform * uModelTransform * vec4(aSquareVertexPosition, 1.0);
 }
