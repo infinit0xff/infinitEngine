225f43848111d433f4542ee6d335276262b36ef8
infinit0xff <infinit0xff@gmail.com>
2019-09-06 23:08:24 -0700

    renderable supports transformable objects, working in demo.js.

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -12,7 +12,7 @@ function Demo(htmlCanvasID) {
         "src/glsl_shaders/simple_fs.glsl"
     );
     
-    // create Renderable objects
+    // create renderable objects
     this.ivRedSq = new Renderable(this.ivConstColorShader);
     this.ivRedSq.setColor([0.8, 0.2, 0.2, 1]);
     this.ivBlueSq = new Renderable(this.ivConstColorShader);
@@ -25,18 +25,23 @@ function Demo(htmlCanvasID) {
     var xform = mat4.create();
     
     // compute red transform operator
-    mat4.translate(xform, xform, vec3.fromValues(-0.25, 0.25, 0.0));
-    mat4.rotateZ(xform, xform, 0.2);
-    mat4.scale(xform, xform, vec3.fromValues(1.2, 1.2, 1.0));
+    this.ivRedSq.getXform().setPosition(-0.25, 0.25);
+    this.ivRedSq.getXform().setRotationInRad(0.2); // In Radians
+    this.ivRedSq.getXform().setSize(1.2, 1.2);
     this.ivRedSq.draw(xform);
 
-    // restart
-    mat4.identity(xform);
-
     // compute blue transform operator
-    mat4.translate(xform, xform, vec3.fromValues(-0.25, 0.25, 0.0));
-    mat4.rotateZ(xform, xform, -0.785);
-    mat4.scale(xform, xform, vec3.fromValues(0.4, 0.4, 1.0));
+    //
+    // to show alternative to setPosition
+    this.ivBlueSq.getXform().setXPos(0.25);
+     // possible to setX/Y separately
+    this.ivBlueSq.getXform().setYPos(-0.25);
+    // degree of ratation ex: 45
+    this.ivBlueSq.getXform().setRotationInDegree(45);
+    // show alternative to setSize
+    this.ivBlueSq.getXform().setWidth(0.4);
+    // possible to set width/height separately
+    this.ivBlueSq.getXform().setHeight(0.4);
     this.ivBlueSq.draw(xform);
 
     // draw with activated geometry and shader
