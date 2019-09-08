def47b9830b28b22607988c9d5a38572c609c6bd
infinit0xff <infinit0xff@gmail.com>
2019-09-07 21:59:47 -0700

    abstract scene object into scene.js

---
+++ src/engine/scene.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// constructor
+function Scene() {}
+
+Scene.prototype.initialize = function() {
+    // called from game loop after loading is done
+};
+
+Scene.prototype.loadScene = function() {
+    // called from engineCore.startScene()
+}
+
+Scene.prototype.unloadScene = function() { };
+
+Scene.prototype.update = function() { };
+
+Scene.prototype.draw = function() { };
