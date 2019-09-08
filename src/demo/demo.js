a4e04ebb7f96a9e8606e2c3dbbc63fc2fce5bfdd
infinit0xff <infinit0xff@gmail.com>
2019-09-08 02:05:56 -0700

    move square left / right, left to cycle scenes.

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -1,36 +1,55 @@
 "use strict";
 
 function Demo() {
-    
-    // scene file name
-    this.kSceneFile = "assets/scene.xml"
-    
+
+     // scene file name
+     this.kSceneFile = "assets/scene.xml"
+     
     // all squares
     // renderable objects
     this.ivSqSet = [];
 
     // camera to view the scene
     this.ivCamera = null;
+
+    // hero and support objects
+    this.ivHero = null;
+    this.ivSupport = null;
 }
 
+infinitEngine.Core.inheritPrototype(Demo, Scene);
 
-Demo.prototype.loadScene = function() {
-    infinitEngine.TextFileLoader.loadTextFile(this.kSceneFile,
-        infinitEngine.TextFileLoader.eTextFileType.eXMLFile);
-};
+// Demo.prototype.loadScene = function() {
+//     infinitEngine.TextFileLoader.loadTextFile(this.kSceneFile,
+//         infinitEngine.TextFileLoader.eTextFileType.eXMLFile);
+// };
 
 Demo.prototype.unloadScene = function() {
-    infinitEngine.TextFileLoader.unloadTextFile(this.kSceneFile);
+    var nextLevel = new BlueLevel();
+    infinitEngine.Core.startScene(nextLevel);
 };
 
 Demo.prototype.initialize = function() {
-    var sceneParser = new SceneFileParser(this.kSceneFile);
+    // Step A: set up the cameras
+    this.ivCamera = new Camera(
+        vec2.fromValues(20, 60),   // position of the camera
+        20,                        // width of camera
+        [20, 40, 600, 300]         // viewport (orgX, orgY, width, height)
+    );
+    this.ivCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
+            // sets the background to gray
 
-    // Step A: Parse the camera
-    this.ivCamera = sceneParser.parseCamera();
+    // Step B: Create the support object in red
+    this.ivSupport = new Renderable(infinitEngine.DefaultResources.getConstColorShader());
+    this.ivSupport.setColor([0.8, 0.2, 0.2, 1]);
+    this.ivSupport.getXform().setPosition(20, 60);
+    this.ivSupport.getXform().setSize(5, 5);
 
-    // Step B: Parse for all the squares
-    sceneParser.parseSquares(this.ivSqSet);
+    // Setp C: Create the hero object in blue
+    this.ivHero = new Renderable(infinitEngine.DefaultResources.getConstColorShader());
+    this.ivHero.setColor([0, 0, 1, 1]);
+    this.ivHero.getXform().setPosition(20, 60);
+    this.ivHero.getXform().setSize(2, 3);
 };
 
 Demo.prototype.draw = function() {
@@ -41,37 +60,29 @@ Demo.prototype.draw = function() {
     this.ivCamera.setupViewProjection();
   
     // draw all squares
-    var i;
-    for (i = 0; i < this.ivSqSet.length; i++) {
-        this.ivSqSet[i].draw(this.ivCamera.getVPMatrix());
-    }
+     this.ivSupport.draw(this.ivCamera.getVPMatrix());
+     this.ivHero.draw(this.ivCamera.getVPMatrix());
 };
 
 Demo.prototype.update = function() {
     // simple game for now only moves the blue pulses the red
     
     // move the blue square
-    var xform = this.ivSqSet[0].getXform();
     var deltaX = 0.05;
-    
-    if (infinitEngine.Input.isKeyPressed(infinitEngine.Input.keys.Right)) {
-        if (xform.getXPos() > 30) {
-            xform.setPosition(10, 60);
-        }
+    var xform = this.ivHero.getXform();
 
-        xform.incXPosBy(deltaX);
+   // support hero movements
+   if (infinitEngine.Input.isKeyPressed(infinitEngine.Input.keys.Right)) {
+    xform.incXPosBy(deltaX);
+    if (xform.getXPos() > 30) { // this is the right-bound of the window
+        xform.setPosition(12, 60);
     }
+}
 
-    if (infinitEngine.Input.isKeyPressed(infinitEngine.Input.keys.Up)) {
-        xform.incRotationByDegree(1);
+if (infinitEngine.Input.isKeyPressed(infinitEngine.Input.keys.Left)) {
+    xform.incXPosBy(-deltaX);
+    if (xform.getXPos() < 11) {  // this is the left-bound of the window
+        infinitEngine.GameLoop.stop();
     }
-
-    
-    xform = this.ivSqSet[1].getXform();
-    if (infinitEngine.Input.isKeyPressed(infinitEngine.Input.keys.Down)) {
-        if(xform.getWidth() > 5) {
-            xform.setSize(2, 2);
-        }
-        xform.incSizeBy(0.05);
-    }
+}
 };
