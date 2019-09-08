4fd34ca04ee3f0c34d9ad81aac94af226f301685
infinit0xff <infinit0xff@gmail.com>
2019-09-07 20:35:34 -0700

    intergrated game resource loading in demo.js

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -2,45 +2,35 @@
 
 function Demo() {
     
-    // square variables
-    this.ivBlueSq = null;
-    this.ivRedSq = null;
+    // scene file name
+    this.kSceneFile = "assets/scene.xml"
+    
+    // all squares
+    // renderable objects
+    this.ivSqSet = [];
 
     // camera to view the scene
     this.ivCamera = null;
 }
 
+
+Demo.prototype.loadScene = function() {
+    infinitEngine.TextFileLoader.loadTextFile(this.kSceneFile,
+        infinitEngine.TextFileLoader.eTextFileType.eXMLFile);
+};
+
+Demo.prototype.unloadScene = function() {
+    infinitEngine.TextFileLoader.unloadTextFile(this.kSceneFile);
+};
+
 Demo.prototype.initialize = function() {
-    // setup the camera
-    this.ivCamera = new Camera(
-        vec2.fromValues(20, 60),    // center
-        20,                         // width
-        [20, 40, 600, 300]          // viewport ( orgX, orgY, width, height)
-    );
+    var sceneParser = new SceneFileParser(this.kSceneFile);
 
-    // set background to dark gray
-    this.ivCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
-    
-    // create renderable objects
-    var constColorShader = infinitEngine.DefaultResources.getConstColorShader();
-    this.ivRedSq = new Renderable(constColorShader);
-    this.ivRedSq.setColor([0.8, 0.2, 0.2, 1]);
-    this.ivBlueSq = new Renderable(constColorShader);
-    this.ivBlueSq.setColor([0.2, 0.4, 0.8, 1]);
-    
-     // draw the blue square
-    // centre blue, slightly rotated square
-    this.ivBlueSq.getXform().setPosition(20, 60);
-    this.ivBlueSq.getXform().setRotationInRad(0.2); // In Radians
-    this.ivBlueSq.getXform().setSize(5, 5);
+    // Step A: Parse the camera
+    this.ivCamera = sceneParser.parseCamera();
 
-    // draw the center and the corner squares
-    // centre red square
-    this.ivRedSq.getXform().setPosition(20, 60);
-    this.ivRedSq.getXform().setSize(2, 2);
-
-    // start the game loop
-    infinitEngine.GameLoop.start(this);
+    // Step B: Parse for all the squares
+    sceneParser.parseSquares(this.ivSqSet);
 };
 
 Demo.prototype.draw = function() {
@@ -50,35 +40,38 @@ Demo.prototype.draw = function() {
     // starts the drawing by activating the camera
     this.ivCamera.setupViewProjection();
   
-    // activate the blue shader to draw
-    this.ivBlueSq.draw(this.ivCamera.getVPMatrix());
-
-    // activate the red shader to draw
-    this.ivRedSq.draw(this.ivCamera.getVPMatrix());
+    // draw all squares
+    var i;
+    for (i = 0; i < this.ivSqSet.length; i++) {
+        this.ivSqSet[i].draw(this.ivCamera.getVPMatrix());
+    }
 };
 
 Demo.prototype.update = function() {
     // simple game for now only moves the blue pulses the red
     
     // move the blue square
-    var blueXform = this.ivBlueSq.getXform();
-    var redXform = this.ivRedSq.getXform();
+    var xform = this.ivSqSet[0].getXform();
     var deltaX = 0.05;
     
     if (infinitEngine.Input.isKeyPressed(infinitEngine.Input.keys.Right)) {
-        if (blueXform.getXPos() > 30)
-            blueXform.setPosition(10, 60);
-        blueXform.incXPosBy(deltaX);
+        if (xform.getXPos() > 30) {
+            xform.setPosition(10, 60);
+        }
+
+        xform.incXPosBy(deltaX);
     }
 
     if (infinitEngine.Input.isKeyPressed(infinitEngine.Input.keys.Up)) {
-        blueXform.incRotationByDegree(1);
+        xform.incRotationByDegree(1);
     }
 
-
+    
+    xform = this.ivSqSet[1].getXform();
     if (infinitEngine.Input.isKeyPressed(infinitEngine.Input.keys.Down)) {
-        if(redXform.getWidth() > 5)
-            redXform.setSize(2, 2);
-        redXform.incSizeBy(0.05);
+        if(xform.getWidth() > 5) {
+            xform.setSize(2, 2);
+        }
+        xform.incSizeBy(0.05);
     }
 };
