1437b9b66729b47b13f8ddae5a88766522eefd88
infinit0xff <infinit0xff@gmail.com>
2019-09-11 07:37:42 -0700

    defined buffer texture coordinates for the glsl shaders.

--- src/engine/core/vertex_buffer.js
+++ src/engine/core/vertex_buffer.js
@@ -5,6 +5,11 @@ var infinitEngine = infinitEngine || {};
 // vertex object
 infinitEngine.VertexBuffer = (function() {
 
+    var ivSquareVertexBuffer = null;
+
+    // reference to the texture positions for the square vertices in the gl context
+    var ivTextureCoordBuffer = null;
+
     // define vertices
     var verticesOfSquare = [
         0.5, 0.5, 0.0,
@@ -12,9 +17,14 @@ infinitEngine.VertexBuffer = (function() {
         0.5, -0.5, 0.0,
         -0.5, -0.5, 0.0
     ];
-    var ivSquareVertexBuffer = null;
-    
-    var getGLVertexRef = function() { return ivSquareVertexBuffer; };
+
+    // texture cooridnates
+    var textureCoordinates = [
+        1.0, 1.0,
+        0.0, 1.0,
+        1.0, 0.0,
+        0.0, 0.0
+    ];
 
     var initialize = function() {
         var gl = infinitEngine.Core.getGL();
@@ -28,12 +38,28 @@ infinitEngine.VertexBuffer = (function() {
         // load vertices into buffer
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verticesOfSquare),
         gl.STATIC_DRAW);
+
+        // create a buffer on the gGL context for our vertex positions
+        ivTextureCoordBuffer = gl.createBuffer();
+
+        // activate textureCoordBuffer
+        gl.bindBuffer(gl.ARRAY_BUFFER, ivTextureCoordBuffer);
+
+        // Loads textureCoordinates into the buffer
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
+    
     };
+
+    // getters
+    var getGLVertexRef = function() { return ivSquareVertexBuffer; };
+    var getGLTexCoordRef = function () { return ivTextureCoordBuffer; };
 
     var ivPublic = {
         initialize: initialize,
-        getGLVertexRef: getGLVertexRef
+        getGLVertexRef: getGLVertexRef,
+        getGLTexCoordRef: getGLTexCoordRef
     };
+    
     return ivPublic;
 
 }()); //create single instance
