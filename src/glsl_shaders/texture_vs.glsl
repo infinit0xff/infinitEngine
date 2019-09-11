de52297490a9094fe084e9facf37e28dbcb680dd
infinit0xff <infinit0xff@gmail.com>
2019-09-11 07:29:16 -0700

    defined glsl texture vertex and fragment shaders.

---
+++ src/glsl_shaders/texture_vs.glsl
@@ -0,0 +1,24 @@
+// vertex shader 
+
+// vertex shader expects one vertex position
+attribute vec3 aSquareVertexPosition;
+
+// texture coordinate attribute
+attribute vec2 aTextureCoordinate;
+
+// texture coordinate that maps image to the square
+varying vec2 vTexCoord;
+
+// to transform the vertex position
+uniform mat4 uModelTransform;
+uniform mat4 uViewProjTransform;
+
+void main(void) { 
+    // convert the vec3 into vec4 for scan conversion and
+    // transform by uModelTransform and uViewProjTransform before
+    // assign to gl_Position to pass the vertex to the fragment shader
+    gl_Position = uViewProjTransform * uModelTransform * vec4(aSquareVertexPosition, 1.0); 
+    
+    // pass the texture coordinate to the fragment shader
+    vTexCoord = aTextureCoordinate;
+}
