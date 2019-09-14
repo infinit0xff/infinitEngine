c08ba1bcadf229a055f2093b3d0dde06890250ce
infinit0xff <infinit0xff@gmail.com>
2019-09-13 23:04:12 -0700

    modified TextureRenderable to support per-pixel collision.

--- src/engine/renderables/texture_renderable.js
+++ src/engine/renderables/texture_renderable.js
@@ -4,8 +4,25 @@ function TextureRenderable(myTexture) {
     Renderable.call(this);
     Renderable.prototype.setColor.call(this, [1, 1, 1, 0]); // Alpha of 0: switch off tinting of texture
     Renderable.prototype._setShader.call(this, infinitEngine.DefaultResources.getTextureShader());
-    this.ivTexture = myTexture;          // texture for this object, cannot be a "null"
+    
+    // texture for this object, cannot be a "null"
+    this.ivTexture = myTexture;
+    
+    // these two instance variables are to cache texture information
+    // for supporting per-pixel accurate collision
+    this.ivTextureInfo = null;
+    this.ivColorArray = null;
+    
+    // defined for subclass to override
+    this.ivTexWidth = 0;
+    this.ivTexHeight = 0;
+    this.ivTexLeftIndex = 0;
+    this.ivTexBottomIndex = 0;
+
+    // set texture
+    this.setTexture(myTexture);
 }
+
 infinitEngine.Core.inheritPrototype(TextureRenderable, Renderable);
 
 TextureRenderable.prototype.draw = function (aCamera) {
@@ -15,4 +32,14 @@ TextureRenderable.prototype.draw = function (aCamera) {
 };
 
 TextureRenderable.prototype.getTexture = function () { return this.ivTexture; };
-TextureRenderable.prototype.setTexture = function (t) { this.ivTexture = t; };
+TextureRenderable.prototype.setTexture = function (newTexture) {
+    this.ivTexture = newTexture;
+    // these two instance variables are to cache texture information
+    // for supporting per-pixel accurate collision
+    this.ivTextureInfo = infinitEngine.Textures.getTextureInfo(newTexture);
+    this.ivColorArray = null;
+    // defined for subclass to override
+    this.ivTexWidth = this.ivTextureInfo.ivWidth;
+    this.ivTexHeight = this.ivTextureInfo.ivHeight;
+    this.ivTexLeftIndex = 0;
+    this.ivTexBottomIndex = 0;};
