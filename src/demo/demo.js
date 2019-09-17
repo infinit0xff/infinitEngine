41f65a77a57b0a3fac2bb60b5cb7467e9821e4c4
infinit0xff <infinit0xff@gmail.com>
2019-09-17 11:02:44 -0700

    updated demo.js to test multiple cameras.

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -7,7 +7,8 @@ function Demo() {
     
     // the camera to view the scene
     this.ivCamera = null;
-
+    this.ivHeroCam = null;
+    this.ivBrainCam = null;
     this.ivBg = null;
 
     this.ivMsg = null;
@@ -40,21 +41,38 @@ Demo.prototype.unloadScene = function () {
 Demo.prototype.initialize = function () {
     // set up the cameras
     this.ivCamera = new Camera(
-        vec2.fromValues(50, 37.5), // position of the camera
+        vec2.fromValues(50, 36), // position of the camera
         100,                       // width of camera
         [0, 0, 640, 480]           // viewport (orgX, orgY, width, height)
     );
-    this.ivCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
-            // sets the background to gray
 
-    // Large background image
+    // sets the background to gray
+    this.ivCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
+
+    this.ivHeroCam = new Camera(
+        vec2.fromValues(50, 30),    // will be updated at each cycle to point to hero
+        20,
+        [490, 330, 150, 150],
+        2                           // viewport bounds
+    );
+    this.ivHeroCam.setBackgroundColor([0.5, 0.5, 0.5, 1]);
+    this.ivBrainCam = new Camera(
+        vec2.fromValues(50, 30),    // will be updated at each cycle to point to the brain
+        10,
+        [0, 330, 150, 150],
+        2                           // viewport bounds
+    );
+    this.ivBrainCam.setBackgroundColor([1, 1, 1, 1]);
+    this.ivBrainCam.configInterpolation(0.7, 10);
+
+    // large background image
     var bgR = new SpriteRenderable(this.kBg);
     bgR.setElementPixelPositions(0, 1024, 0, 1024);
     bgR.getXform().setSize(150, 150);
     bgR.getXform().setPosition(50, 35);
     this.ivBg = new GameObject(bgR);
 
-    // Objects in the scene
+    // objects in the scene
     this.ivBrain = new Brain(this.kMinionSprite);
     this.ivHero = new Hero(this.kMinionSprite);
     this.ivPortal = new TextureObject(this.kMinionPortal, 50, 30, 10, 10);
@@ -68,24 +86,27 @@ Demo.prototype.initialize = function () {
     this.ivMsg.setTextHeight(3);
 };
 
+Demo.prototype.drawCamera = function (camera) {
+    camera.setupViewProjection();
+    this.ivBg.draw(camera);
+    this.ivHero.draw(camera);
+    this.ivBrain.draw(camera);
+    this.ivPortal.draw(camera);
+    this.ivLMinion.draw(camera);
+    this.ivRMinion.draw(camera);
+};
+
 // this is the draw function, make sure to setup proper drawing environment, and more
 // importantly, make sure to _NOT_ change any state.
 Demo.prototype.draw = function () {
     // clear the canvas
     infinitEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
 
-    // activate the drawing Camera
-    this.ivCamera.setupViewProjection();
-
-  
-    // Step  C: Draw everything
-    this.ivBg.draw(this.ivCamera);
-    this.ivHero.draw(this.ivCamera);
-    this.ivBrain.draw(this.ivCamera);
-    this.ivPortal.draw(this.ivCamera);
-    this.ivLMinion.draw(this.ivCamera);
-    this.ivRMinion.draw(this.ivCamera);
-    this.ivMsg.draw(this.ivCamera);
+    // Step  B: Draw with all three cameras
+    this.drawCamera(this.ivCamera);
+    this.ivMsg.draw(this.ivCamera);   // only draw status in the main camera
+    this.drawCamera(this.ivHeroCam);
+    this.drawCamera(this.ivBrainCam);
 };
 
 // the update function, updates the application state. Make sure to _NOT_ draw
@@ -96,6 +117,9 @@ Demo.prototype.update = function () {
     
     // for smoother camera movements
     this.ivCamera.update();
+    
+    this.ivHeroCam.update();
+    this.ivBrainCam.update();
 
     this.ivLMinion.update();
     this.ivRMinion.update();
@@ -109,14 +133,14 @@ Demo.prototype.update = function () {
         infinitEngine.Input.keys.Right
     );
 
-    // Brain chasing the hero
+    // brain chasing the hero
     var h = [];
     if (!this.ivHero.pixelTouches(this.ivBrain, h)) {
         this.ivBrain.rotateObjPointTo(this.ivHero.getXform().getPosition(), 0.01);
         GameObject.prototype.update.call(this.ivBrain);
     }
 
-    // Pan camera to object
+    // pan camera to object
     if (infinitEngine.Input.isKeyClicked(infinitEngine.Input.keys.L)) {
         this.ivFocusObj = this.ivLMinion;
         this.ivChoice = 'L';
@@ -159,6 +183,18 @@ Demo.prototype.update = function () {
     if (infinitEngine.Input.isKeyClicked(infinitEngine.Input.keys.Q)) {
         this.ivCamera.shake(-2, -2, 20, 30);
     }
+
+     // set the hero and brain cams    
+     this.ivHeroCam.panTo(this.ivHero.getXform().getXPos(), this.ivHero.getXform().getYPos());
+     this.ivBrainCam.panTo(this.ivBrain.getXform().getXPos(), this.ivBrain.getXform().getYPos());
+ 
+     // move the hero cam viewport just to show it is possible
+     var v = this.ivHeroCam.getViewport();
+     v[0] += 1;
+     if (v[0] > 500) {
+         v[0] = 0;
+     }
+     this.ivHeroCam.setViewport(v);
 
     this.ivMsg.setText(msg + this.ivChoice);
 };
