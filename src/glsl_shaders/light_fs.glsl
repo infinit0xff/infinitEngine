9da98255e53ac8c562face8dba06bb95404abb94
infinit0xff <infinit0xff@gmail.com>
2019-09-17 23:57:00 -0700

    support for distance attenuation, cutoffs, multiple light sources.

--- src/glsl_shaders/light_fs.glsl
+++ src/glsl_shaders/light_fs.glsl
@@ -13,14 +13,54 @@ uniform vec4 uGlobalAmbientColor; // this is shared globally
 uniform float uGlobalAmbientIntensity;
 
 // light information
-uniform bool uLightOn;
-uniform vec4 uLightColor;
-uniform vec3 uLightPosition;   // in pixel space!
-uniform float uLightRadius;    // in pixel space!
+#define kGLSLuLightArraySize 4
+    // GLSL Fragment shader requires loop control
+    // variable to be a constant number. This number 4
+    // says, this fragment shader will _ALWAYS_ process
+    // all 4 light sources. 
+    // ***********WARNING***********************
+    // this number must correspond to the constant with
+    // the same name defined in light_shader.js file.
+    // ***********WARNING**************************
+    // to change this number MAKE SURE: to update the 
+    //     kGLSLuLightArraySize
+    // defined in light_shader.js file.
+
+struct Light  {
+    vec3 Position;   // in pixel space!
+    vec4 Color;
+    float Near;     // distance in pixel space
+    float Far;     // distance in pixel space
+    float Intensity;
+    bool  IsOn;
+};
+
+// maximum array of lights this shader supports
+uniform Light uLights[kGLSLuLightArraySize];
 
 // the "varying" keyword is for signifing that the texture coordinate will be
 // interpolated and thus varies. 
 varying vec2 vTexCoord;
+
+
+vec4 LightEffect(Light lgt)
+{
+    vec4 result = vec4(0);
+    float atten = 0.0;
+    float dist = length(lgt.Position.xyz - gl_FragCoord.xyz);
+    if (dist <= lgt.Far) {
+        if (dist <= lgt.Near)
+            atten = 1.0;  //  no attenuation
+        else {
+            // simple quadratic drop off
+            float n = dist - lgt.Near;
+            float d = lgt.Far - lgt.Near;
+            atten = smoothstep(0.0, 1.0, 1.0-(n*n)/(d*d)); // blended attenuation
+        }   
+    }
+    result = atten * lgt.Intensity * lgt.Color;
+    return result;
+}
 
 void main(void)  {
     // simple tint based on uPixelColor setting
@@ -28,10 +68,12 @@ void main(void)  {
     vec4 lgtResults = uGlobalAmbientIntensity * uGlobalAmbientColor;
 
     // now decide if we should illuminate by the light
-    if (uLightOn && (textureMapColor.a > 0.0)) {
-        float dist = length(uLightPosition.xyz - gl_FragCoord.xyz);
-        if (dist <= uLightRadius)
-            lgtResults += uLightColor;
+    if (textureMapColor.a > 0.0) {
+        for (int i=0; i<kGLSLuLightArraySize; i++) { 
+            if (uLights[i].IsOn) { 
+                lgtResults +=  LightEffect(uLights[i]);
+            }
+        }
     }
     lgtResults *= textureMapColor;
 
