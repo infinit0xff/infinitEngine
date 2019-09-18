6744ea2f5999772223f2f97579f322a1f54e8082
infinit0xff <infinit0xff@gmail.com>
2019-09-18 00:03:52 -0700

    modified LightShader object for array of lights.

--- src/engine/shaders/light_shader.js
+++ src/engine/shaders/light_shader.js
@@ -1,57 +1,44 @@
 "use strict";
 
-// constructor 
 function LightShader(vertexShaderPath, fragmentShaderPath) {
     // call super class constructor
     SpriteShader.call(this, vertexShaderPath, fragmentShaderPath);  // call SimpleShader constructor
 
-    // glsl uniform position references
-    this.ivColorRef = null;
-    this.ivPosRef = null;
-    this.ivRadiusRef = null;
-    this.ivIsOnRef = null;
+    this.ivLights = null;  // lights from the Renderable
 
-    // light source in the game engine
-    this.ivLight = null;
-
-    // create the references to these uniforms in the LightShader
-    var shader = this.ivCompiledShader;
-    var gl = infinitEngine.Core.getGL();
-    this.ivColorRef = gl.getUniformLocation(shader, "uLightColor");
-    this.ivPosRef = gl.getUniformLocation(shader, "uLightPosition");
-    this.ivRadiusRef = gl.getUniformLocation(shader, "uLightRadius");
-    this.ivIsOnRef = gl.getUniformLocation(shader, "uLightOn");
+    //*******WARNING***************
+    // this number MUST correspond to the GLSL uLight[] array size (for LightFS.glsl)
+    //*******WARNING********************
+    this.kGLSLuLightArraySize = 4;  // <-- make sure this is the same as LightFS.glsl
+    this.ivShaderLights = [];
+    var i, ls;
+    for (i = 0; i < this.kGLSLuLightArraySize; i++) {
+        ls = new ShaderLightAtIndex(this.ivCompiledShader, i);
+        this.ivShaderLights.push(ls);
+    }
 }
 infinitEngine.Core.inheritPrototype(LightShader, SpriteShader);
 
-// overriding the Activation of the shader for rendering
+// overriding the activation of the shader for rendering
 LightShader.prototype.activateShader = function (pixelColor, aCamera) {
     // first call the super class's activate
     SpriteShader.prototype.activateShader.call(this, pixelColor, aCamera);
 
     // now push the light information to the shader
-    if (this.ivLight !== null) {
-        this._loadToShader(aCamera);
-    } else {
-         // switch off the light!
-        infinitEngine.Core.getGL().uniform1i(this.ivIsOnRef, false);
+    var numLight = 0;
+    if (this.ivLights !== null) {
+        while (numLight < this.ivLights.length) {
+            this.ivShaderLights[numLight].loadToShader(aCamera, this.ivLights[numLight]);
+            numLight++;
+        }
+    }
+    // switch off the left over ones.
+    while (numLight < this.kGLSLuLightArraySize) {
+        this.ivShaderLights[numLight].switchOffLight();
+        numLight++;
     }
 };
 
-LightShader.prototype.setLight = function (l) {
-    this.ivLight = l;
-};
-
-LightShader.prototype._loadToShader = function (aCamera) {
-    var gl = infinitEngine.Core.getGL();
-    gl.uniform1i(this.ivIsOnRef, this.ivLight.isLightOn());
-    if (this.ivLight.isLightOn()) {
-        var p = aCamera.wcPosToPixel(this.ivLight.getPosition());
-        var r = aCamera.wcSizeToPixel(this.ivLight.getRadius());
-        var c = this.ivLight.getColor();
-
-        gl.uniform4fv(this.ivColorRef, c);
-        gl.uniform3fv(this.ivPosRef, vec3.fromValues(p[0], p[1], p[2]));
-        gl.uniform1f(this.ivRadiusRef, r);
-    }
+LightShader.prototype.setLights = function (l) {
+    this.ivLights = l;
 };
