91ba01bd549488fe5d12a93392a4e366082b8375
infinit0xff <infinit0xff@gmail.com>
2019-09-18 00:04:48 -0700

    LightRenderable object support multiple light sources.

--- src/engine/renderables/light_renderable.js
+++ src/engine/renderables/light_renderable.js
@@ -5,19 +5,19 @@ function LightRenderable(myTexture) {
     Renderable.prototype._setShader.call(this, infinitEngine.DefaultResources.getLightShader());
 
     // here is the light source
-    this.ivLight = null;
+    this.ivLights = [];
 }
 infinitEngine.Core.inheritPrototype(LightRenderable, SpriteAnimateRenderable);
 
 LightRenderable.prototype.draw = function (aCamera) {
-    this.ivShader.setLight(this.ivLight);
+    this.ivShader.setLights(this.ivLights);
     SpriteAnimateRenderable.prototype.draw.call(this, aCamera);
 };
 
-LightRenderable.prototype.getLight = function () {
-    return this.ivLight;
+LightRenderable.prototype.getLightAt = function (index) {
+    return this.ivLights[index];
 };
 
 LightRenderable.prototype.addLight = function (l) {
-    this.ivLight = l;
+    this.ivLights.push(l);
 };
