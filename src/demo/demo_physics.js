3a29e2449314c098f74e61bf47329e559de29600
infinit0xff <infinit0xff@gmail.com>
2019-09-22 12:14:15 -0700

    updated demo_physics.js to test particle system.

--- src/demo/demo_physics.js
+++ src/demo/demo_physics.js
@@ -20,43 +20,9 @@ Demo.prototype._physicsSimulation = function() {
     // Hero DyePack
     infinitEngine.Physics.processObjSet(this.ivHero, this.ivAllDyePacks);
     
-    // all rigid shapes
-    infinitEngine.Physics.processSetSet(this.ivAllRigidShapes, this.ivAllPlatforms);
-    infinitEngine.Physics.processSelfSet(this.ivAllRigidShapes);
-    
-    // add rigid shapes if keys are pressed
-    if (infinitEngine.Input.isKeyPressed(infinitEngine.Input.keys.Z)) {
-        var c = new Minion(this.kMinionSprite, this.ivCamera.mouseWCX(), this.ivCamera.mouseWCY());
-        var p = c.getPhysicsComponent();
-        p.setAcceleration(infinitEngine.Physics.getSystemtAcceleration());
-        p.setColor([1, 1, 1, 1]);
-        this.ivAllRigidShapes.addToSet(c);
-    }
-    if (infinitEngine.Input.isKeyPressed(infinitEngine.Input.keys.C)) {
-        // Give all rigid shape a random velocity
-        var i = 0, s, v;
-        for (i=0; i<this.ivAllRigidShapes.size(); i++) {
-            s = this.ivAllRigidShapes.getObjectAt(i).getPhysicsComponent();
-            v = s.getVelocity();
-            v[0] += (Math.random()-0.5)*10;
-            v[1] += (Math.random()-0.5)*10;
-        }
-    }
-    if (infinitEngine.Input.isKeyPressed(infinitEngine.Input.keys.V)) {
-        this.ivAllRigidShapes = new GameObjectSet();
-    }
-    if (infinitEngine.Input.isKeyPressed(infinitEngine.Input.keys.X)) {
-        // find the current rigid shape under the mouse and move it with the mouse
-        var i = 0, s;
-        var pos = vec2.fromValues(this.ivCamera.mouseWCX(), this.ivCamera.mouseWCY());
-        var found = false;
-        while ((i<this.ivAllRigidShapes.size()) && (!found)) {
-            s = this.ivAllRigidShapes.getObjectAt(i).getPhysicsComponent();
-            found = s.containsPos(pos);
-            i++;
-        }
-        if (found) {
-            s.setPosition(pos[0], pos[1]);
-        }
-    }
+   // particle system collisions
+   infinitEngine.Particle.processObjSet(this.ivHero, this.ivAllParticles);
+   infinitEngine.Particle.processSetSet(this.ivAllMinions, this.ivAllParticles);
+   infinitEngine.Particle.processSetSet(this.ivAllPlatforms, this.ivAllParticles);
+
 };
