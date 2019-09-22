4516c7a6be65dbdf8b26141a05bafb393c9cf526
infinit0xff <infinit0xff@gmail.com>
2019-09-21 21:35:39 -0700

    added line vertex buffer reference to vertex_buffer.js.

--- src/engine/core/engine_vertexbuffer.js
+++ src/engine/core/engine_vertexbuffer.js
@@ -26,6 +26,15 @@ infinitEngine.VertexBuffer = (function() {
         0.0, 0.0
     ];
 
+    // this is to support the debugging of physics engine
+    var verticesOfLine = [
+        0.5, 0.5, 0.0,
+        -0.5, -0.5, 0.0
+    ];
+
+    // reference to positions for the line vertices in the gl context
+    var ivLineVertexBuffer = null;
+
     var initialize = function() {
         var gl = infinitEngine.Core.getGL();
         
@@ -48,22 +57,35 @@ infinitEngine.VertexBuffer = (function() {
         // Loads textureCoordinates into the buffer
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
     
+        // Create a buffer on the gGL context for our vertex positions
+        ivLineVertexBuffer = gl.createBuffer();
+
+        // Connect the vertexBuffer to the ARRAY_BUFFER global gl binding point.
+        gl.bindBuffer(gl.ARRAY_BUFFER, ivLineVertexBuffer);
+ 
+        // Put the verticesOfSquare into the vertexBuffer, as non-changing drawing data (STATIC_DRAW)
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verticesOfLine), gl.STATIC_DRAW);
+   
     };
 
     var cleanUp = function() {
         var gl = infinitEngine.Core.getGL();
         gl.deleteBuffer(ivSquareVertexBuffer);
         gl.deleteBuffer(ivTextureCoordBuffer);
+        gl.deleteBuffer(ivLineVertexBuffer);
+
     };
 
     // getters
     var getGLVertexRef = function() { return ivSquareVertexBuffer; };
     var getGLTexCoordRef = function () { return ivTextureCoordBuffer; };
+    var getGLLineVertexRef = function () { return ivLineVertexBuffer; };
 
     var ivPublic = {
         initialize: initialize,
         getGLVertexRef: getGLVertexRef,
         getGLTexCoordRef: getGLTexCoordRef,
+        getGLLineVertexRef: getGLLineVertexRef,
         cleanUp: cleanUp
     };
     
